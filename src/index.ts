0107194ee4bc555f9893ec3168f1c7137fb3783d
Oleksii Volkov <lifecoder.ua@gmail.com>
2024-12-03 19:47:08 +0200

    better preview

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// TODO: enlarge on click
+// TODO: better gallery view
+// TODO: handle non-image payload (pptx, docx, etc)
+
 function inlineAttachments() {
   const attachments = document.querySelectorAll('.hometask-attachments a');
   
@@ -8,6 +12,7 @@ function inlineAttachments() {
     const img = document.createElement('img');
     img.src = href || '';
     img.alt = attachment.textContent || 'Attachment';
+    img.setAttribute('style', "max-width: 80%;");
     
     const parentDiv = attachment.parentElement;
     if (parentDiv) {
